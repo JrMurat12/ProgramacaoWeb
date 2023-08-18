e1fdc1f48fdd45d918317ba6075fb85375d6b493
jefferson.murat <jr-murat@hotmail.com>
2023-08-17 22:18:56 -0300

    ArcWeb 17-08

---
+++ ArquiteturaWEB/aula2/src/main/java/com/example/aula2/Models/Usuario.java
@@ -0,0 +1,28 @@
+package com.example.aula2.Models;
+
+public class Usuario {
+    private Integer codigo;
+    private String nome;
+
+    public Usuario(Integer codigo, String nome) {
+        this.codigo = codigo;
+        this.nome = nome;
+    }
+
+    public Usuario() {
+    }
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
---
+++ ArquiteturaWEB/aula2/src/main/java/com/example/aula2/UsuarioRepository.java
@@ -0,0 +1,24 @@
+package com.example.aula2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.example.aula2.Models.Usuario;
+
+@Repository
+public class UsuarioRepository {
+
+    private List<Usuario> usuarios = new ArrayList<>();
+
+    public boolean inserir(Usuario usuario){
+        //Código para salvar no banco...
+        usuarios.add(usuario);
+        return true;
+    }
+
+    public List<Usuario> obterTodos(){
+        return usuarios;
+    }
+}
