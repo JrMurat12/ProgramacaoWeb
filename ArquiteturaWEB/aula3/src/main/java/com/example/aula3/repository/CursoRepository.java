8e27801bf8a5cce03d310f85095fb958df37e312
jefferson.murat <jr-murat@hotmail.com>
2023-08-24 22:14:59 -0300

    ARC 24-08

---
+++ ArquiteturaWEB/aula3/src/main/java/com/example/aula3/repository/CursoRepository.java
@@ -0,0 +1,54 @@
+package com.example.aula3.repository;
+
+import java.util.List;
+
+import javax.swing.text.html.parser.Entity;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.example.aula3.models.Curso;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import jakarta.transaction.Transactional;
+
+@Repository
+public class CursoRepository {
+    @Autowired
+    private EntityManager entityManager;
+
+    @Transactional
+    public Curso inserir (Curso curso){
+        entityManager.merge(curso);
+        return curso;
+    }
+
+    @Transactional
+    public Curso editar(Curso curso){
+        entityManager.merge(curso);
+        return curso;
+    }
+
+    @Transactional
+    public void excluir(Curso curso){
+        entityManager.remove(curso);
+    }
+
+    @Transactional
+    public void excluir(int id){
+        excluir(entityManager.find(Curso.class, id));
+    }
+
+    public List<Curso> obterTodos() {
+        return entityManager.createQuery("from Curso", Curso.class).getResultList();
+    }
+
+    public List<Curso> obterPorNome(String nome){
+        String jpql = " select c from Curso c where c.nome like :nome";
+        TypedQuery<Curso> query = entityManager.createQuery(jpql, Curso.class);
+        query.setParameter("nome", "%" + nome + "%");
+        return query.getResultList();
+    }
+
+}
