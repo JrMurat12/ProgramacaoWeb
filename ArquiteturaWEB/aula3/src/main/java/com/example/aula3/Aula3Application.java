38508d235c08d49fb26041f96ab78d8229c9b0f4
jefferson.murat <jr-murat@hotmail.com>
2023-09-14 21:14:31 -0300

    Web 14-09

--- ArquiteturaWEB/aula3/src/main/java/com/example/aula3/Aula3Application.java
+++ ArquiteturaWEB/aula3/src/main/java/com/example/aula3/Aula3Application.java
@@ -8,23 +8,35 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import com.example.aula3.models.CategoriaCurso;
 import com.example.aula3.models.Curso;
+import com.example.aula3.repository.CategoriaCursoRepository;
 import com.example.aula3.repository.CursoRepository;
 
 @SpringBootApplication
 public class Aula3Application {
 
 @Bean
-public CommandLineRunner init(@Autowired CursoRepository cursoRepository) {
+public CommandLineRunner init(@Autowired CursoRepository cursoRepository,
+@Autowired CategoriaCursoRepository categoriaCursoRepository) {
 	return args -> {
 		cursoRepository.inserir(
 			new Curso((long)0, "teste2", 2050));
 		cursoRepository.inserir(
 			new Curso((long)0, "teste2", 2050));
 		List<Curso> listaCursos = cursoRepository.obterTodos();
-		// System.out.println("EXEMPLO OBTER POR NOME...!");
-		// listaCursos = cursoRepository.obterPorNome("2");
+		System.out.println("EXEMPLO OBTER POR NOME...!");
+		listaCursos = cursoRepository.obterPorNome("2");
 		listaCursos.forEach(System.out::println);
+
+		System.out.println("EXEMPLO INSERIR CATEGORIA...!");
+		CategoriaCurso c1 = new CategoriaCurso(0, "TI");
+		categoriaCursoRepository.inserir(c1);
+
+		System.out.println("EXEMPLO ATUALIZAR CATEGORIA CURSO...!");
+		listaCursos.get(0).setCategoriaCurso(c1);
+		cursoRepository.inserir(listaCursos.get(0));
+
 	};
 }
 
