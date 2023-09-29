69b8ff2a0760c0adb536ed8dfd211ff0f0984f07
jefferson.murat <jr-murat@hotmail.com>
2023-09-28 22:18:13 -0300

    Arc Web 28-09

--- ArquiteturaWEB/aula3/src/main/java/com/example/aula3/services/CursoServiceImpl.java
+++ ArquiteturaWEB/aula3/src/main/java/com/example/aula3/services/CursoServiceImpl.java
@@ -1,5 +1,8 @@
 package com.example.aula3.services;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.example.aula3.dtos.CursoDTO;
@@ -29,6 +32,19 @@ public class CursoServiceImpl implements CursoService {
         c.setCategoriaCurso(categ);
         c.setNome(cursoDTO.getNome());
         return cursoRepository.save(c);
+    }
+
+    public List<CursoDTO> listarCursos() {
+        List<Curso> cursos = cursoRepository.findAll();
+        List<CursoDTO> cursosDTO = new ArrayList();
+        for(Curso c : cursos){
+            cursosDTO.add(new CursoDTO(
+                c.getId(),
+                c.getNome(),
+                c.getCargaHoraria(),
+                c.getCategoriaCurso() != null ?c.getCategoriaCurso().getId() : 0));
+        }
+        return cursosDTO;
     }
     
 }
