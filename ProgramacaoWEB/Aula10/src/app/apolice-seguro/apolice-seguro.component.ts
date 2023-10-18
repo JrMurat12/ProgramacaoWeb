2a4cbe7f71233c0d4c93fcea65512df9ec0e31a0
jefferson.murat <jr-murat@hotmail.com>
2023-10-18 20:04:18 -0300

    Web 18-10

--- ProgramacaoWEB/Aula10/src/app/apolice-seguro/apolice-seguro.component.ts
+++ ProgramacaoWEB/Aula10/src/app/apolice-seguro/apolice-seguro.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./apolice-seguro.component.css']
 })
 export class ApoliceSeguroComponent implements OnInit {
-
+  nome: string;
+  sexo: string = 'masculino';
+  idade: number = 0;
+  valor: number = 0;
+  resultado: number = 0;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  calcular(){
+    if (this.sexo === 'masculino') {
+      if (this.idade <= 25) {
+        this.resultado = 0.15 * this.valor;
+      } else {
+        this.resultado = 0.10 * this.valor;
+      }
+    } else if (this.sexo === 'feminino') {
+      this.resultado = 0.08 * this.valor;
+    }
   }
 
 }
